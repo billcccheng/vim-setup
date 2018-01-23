d415b432620fd45ffcbdd4aab7d7d9890c43f29b
Bill Cheng <chun-chan.cheng@viasat.com>
2018-01-23 08:58:25 -0600

    TESTING

--- vim-setup.sh
+++ vim-setup.sh
@@ -5,7 +5,7 @@ cp monokai.vim ~/.vim/colors
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git 
+git clone https://github.com/VundleVim/Vundle.vim.git
 
 # Nerdtree Stuffs
 git clone https://github.com/scrooloose/nerdtree.git
@@ -13,7 +13,7 @@ git clone https://github.com/jistr/vim-nerdtree-tabs.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 
 # Better UX for vim
-git clone https://github.com/itchyny/lightline.vim 
+git clone https://github.com/itchyny/lightline.vim
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/mattn/emmet-vim.git
 git clone https://github.com/jiangmiao/auto-pairs.git
@@ -34,3 +34,5 @@ git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/vim-snippets.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/tomtom/tlib_vim.git
+
+#TEST
